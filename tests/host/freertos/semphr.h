51482efbc8e1273d7123334476610acb2538d33c
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-03 11:33:51 +0200

    Complete FreeRTOS mutex host fake

--- tests/host/freertos/semphr.h
+++ tests/host/freertos/semphr.h
@@ -6,7 +6,7 @@
 
 using SemaphoreHandle_t = std::recursive_mutex *;
 
-inline SemaphoreHandle_t xSemaphoreCreateRecursiveMutexStatic(StaticSemaphore_t *storage) {
+inline SemaphoreHandle_t traceHostCreateMutex(StaticSemaphore_t *storage) {
 	if (storage == nullptr) {
 		return nullptr;
 	}
@@ -17,13 +17,21 @@ inline SemaphoreHandle_t xSemaphoreCreateRecursiveMutexStatic(StaticSemaphore_t 
 	return mutex;
 }
 
+inline SemaphoreHandle_t xSemaphoreCreateMutexStatic(StaticSemaphore_t *storage) {
+	return traceHostCreateMutex(storage);
+}
+
+inline SemaphoreHandle_t xSemaphoreCreateRecursiveMutexStatic(StaticSemaphore_t *storage) {
+	return traceHostCreateMutex(storage);
+}
+
 inline void vSemaphoreDelete(SemaphoreHandle_t handle) {
 	if (handle != nullptr) {
 		std::destroy_at(handle);
 	}
 }
 
-inline BaseType_t xSemaphoreTakeRecursive(SemaphoreHandle_t handle, TickType_t timeout) {
+inline BaseType_t xSemaphoreTake(SemaphoreHandle_t handle, TickType_t timeout) {
 	(void)timeout;
 	if (handle == nullptr) {
 		return pdFALSE;
@@ -32,10 +40,18 @@ inline BaseType_t xSemaphoreTakeRecursive(SemaphoreHandle_t handle, TickType_t t
 	return pdTRUE;
 }
 
-inline BaseType_t xSemaphoreGiveRecursive(SemaphoreHandle_t handle) {
+inline BaseType_t xSemaphoreGive(SemaphoreHandle_t handle) {
 	if (handle == nullptr) {
 		return pdFALSE;
 	}
 	handle->unlock();
 	return pdTRUE;
+}
+
+inline BaseType_t xSemaphoreTakeRecursive(SemaphoreHandle_t handle, TickType_t timeout) {
+	return xSemaphoreTake(handle, timeout);
+}
+
+inline BaseType_t xSemaphoreGiveRecursive(SemaphoreHandle_t handle) {
+	return xSemaphoreGive(handle);
 }
